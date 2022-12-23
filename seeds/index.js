ddf148f305e4dec1284bebd76cbb703b1cb494ad
Jagoda Giedrojc <giedrojc.jagoda@gmail.com>
2022-12-23 23:01:25 +0100

    authorization

--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '635acb1f895f3f03288dc54f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://random.imagecdn.app/483251/150',
